fdf112046e076ac5525e98ebb92ca30422db7d35
rctatman <rctatman@users.noreply.github.com>
2020-11-20 10:14:20 -0800

    database queries working

--- actions/actions.py
+++ actions/actions.py
@@ -25,44 +25,47 @@ class QueryResourceType(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        conn = create_connection("../edu_db/resourcesDB")
+        conn = DbQueryingMethods.create_connection(db_file="./edu_db/resourcesDB")
 
         slot_value = tracker.get_slot("resource_type")
         slot_name = "Type"
 
-        get_query_results = select_by_slot(conn,slot_name,slot_value)
-        dispatcher.utter_message(text=get_query_results)
+        get_query_results = DbQueryingMethods.select_by_slot(conn=conn,
+            slot_name=slot_name,slot_value=slot_value)
+        dispatcher.utter_message(text=str(get_query_results))
 
-        return []
+        return 
 
-def create_connection(db_file):
-    """ create a database connection to the SQLite database
-        specified by the db_file
-    :param db_file: database file
-    :return: Connection object or None
-    """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error as e:
-        print(e)
 
-    return conn
+class DbQueryingMethods:
+    def create_connection(db_file):
+        """ create a database connection to the SQLite database
+            specified by the db_file
+        :param db_file: database file
+        :return: Connection object or None
+        """
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+        except Error as e:
+            print(e)
 
-def select_by_slot(conn, slot_name, slot_value):
-    """
-    Query all rows in the tasks table
-    :param conn: the Connection object
-    :return:
-    """
-    cur = conn.cursor()
-    cur.execute(f"""SELECT * FROM eduresource
-                WHERE {slot_name}='{slot_value}'""")
+        return conn
 
-    rows = cur.fetchall()
+    def select_by_slot(conn, slot_name, slot_value):
+        """
+        Query all rows in the tasks table
+        :param conn: the Connection object
+        :return:
+        """
+        cur = conn.cursor()
+        cur.execute(f"""SELECT * FROM eduresource
+                    WHERE {slot_name}='{slot_value}'""")
 
-    if len(list(rows)) < 1:
-        return[("There are no resources matching your query.")]
-    else:
-        for row in random.sample(rows, 1):
-            return[print(f"Try the {(row[4]).lower()} {row[0]} by {row[1]}. You can find it at {row[2]}.")]
+        rows = cur.fetchall()
+
+        if len(list(rows)) < 1:
+            return[("There are no resources matching your query.")]
+        else:
+            for row in random.sample(rows, 1):
+                return f"Try the {(row[4]).lower()} {row[0]} by {row[1]}. You can find it at {row[2]}."
